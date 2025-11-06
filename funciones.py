5984052386dd94e6e4e4c05f0c80cf93bfef6ae2
Juan Franco Cavallieri <54825138+jfcava@users.noreply.github.com>
2025-11-06 18:47:01 -0300

    Funcion 1 COMPLETA - Agregar Pais

--- funciones.py
+++ funciones.py
@@ -1,4 +1,102 @@
 import os
+import csv
+
+NOMBRE_ARCHIVO = "paises.csv"
+
+def escribir_paisDB(pais): #Agregar un titulo al CSV
+    with open(NOMBRE_ARCHIVO, "a", newline="", encoding="utf-8") as archivo:
+        escritor = csv.DictWriter(archivo, fieldnames=["PAIS", "POBLACION", "SUPERFICIE", "CONTINENTE"])
+        escritor.writerow(pais)
+
+def es_numero_positivo(numero):
+    
+    if numero.isdigit():
+        return True
+    else:
+        return False
+
+def es_nombre_vacio(nombre):
+    if nombre == "":
+        return True
+    else:
+        return False
+
+def es_pais_repetido(nombre):
+    
+    paises_disponibles = obtener_paises()
+
+    for pais in paises_disponibles:
+        if nombre.strip().lower() == pais["PAIS"].strip().lower():
+            return True
+    
+    return False
+
+def agregar_pais():
+    
+    while True:
+        limpiar()
+        print("=== AGREGAR NUEVO PAÍS ===")
+
+        nombre = input("Nombre del país: ")
+        
+        # Validar pais repetido
+        while es_pais_repetido(nombre):
+            print("\nEl país ingresado ya se encuentra en la base de datos. Vuelva a intentarlo.")
+            nombre = input("Nombre del país: ")
+
+        # Validar nombre vacio
+        while es_nombre_vacio(nombre):
+            print("\nEl nombre no puede ser vacío.")
+            nombre = input("Nombre del país: ")
+
+        poblacion = input("Población del país: ")
+
+        # Validar numero poblacion
+        while not es_numero_positivo(poblacion):
+            print("\nSolo se aceptan números positivos. Vuelve a intentarlo.")
+            poblacion = input("Población del país: ")
+
+        superficie = input("Superficie del país: ")
+
+        # Validar numero superficie
+        while not es_numero_positivo(superficie):
+            print("\nSolo se aceptan números positivos. Vuelve a intentarlo.")
+            superficie = input("Superficie del país: ")
+        
+        continente = input("Continente del país: ")
+
+        # Validar continente vacio
+        while es_nombre_vacio(continente):
+            print("\nEl nombre no puede ser vacío.")
+            continente = input("Continente del país: ")
+
+        # Cargar pais
+
+        escribir_paisDB({"PAIS": nombre, "POBLACION": int(poblacion), "SUPERFICIE": int(superficie), "CONTINENTE": continente})
+        
+        limpiar()
+        print("País agregado exitosamente.")
+        print("Presione Enter para continuar...")
+        input()
+        limpiar()
+        break
+
+def obtener_paises():
+    paises = []
+    
+    # Si no existe el archivo, lo crea con encabezado
+    if not os.path.exists(NOMBRE_ARCHIVO):
+        with open(NOMBRE_ARCHIVO, "w", newline="", encoding="utf-8") as archivo:
+            escritor = csv.DictWriter(archivo, fieldnames=["PAIS", "POBLACION", "SUPERFICIE", "CONTINENTE"])
+            escritor.writeheader()
+            return paises
+    
+    with open(NOMBRE_ARCHIVO, newline="", encoding="utf-8") as archivo:
+        lector = csv.DictReader(archivo)
+        for fila in lector:
+            paises.append({"PAIS": fila["PAIS"], "POBLACION": int(fila["POBLACION"]), "SUPERFICIE": int(fila["SUPERFICIE"]), "CONTINENTE": fila["CONTINENTE"]})
+    
+    return paises
 
 def limpiar():
     os.system("cls")
@@ -24,7 +122,7 @@ def mostrar_menu():
         opcion = input("Seleccione una opción: ").strip()
         match opcion:
             case "1": # Agregar pais
-                pass                                
+                agregar_pais()                                
             case "2": # Actualizar datos
                 pass
             case "3": # Buscar pais
